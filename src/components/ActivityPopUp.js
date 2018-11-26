b7718ec6c21516c39a5a79b4ca79740c4ce683aa
ningkang <ningkang@iii.org.tw>
2018-11-26 19:53:44 +0800

    Make my tick/progress svg responsive

--- src/components/ActivityPopUp.js
+++ src/components/ActivityPopUp.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Modal, Text, ART, View} from 'react-native';
+import {Modal, Text, ART, Dimensions} from 'react-native';
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as d3 from "d3";
@@ -7,7 +7,7 @@ import * as d3 from "d3";
 const StyledViewContainer = styled.View`
     flex: 1;
     background-color: #36454f;
-    paddingTop: 50;
+    paddingTop: 10%;
 `;
 
 const StyledViewTopBar = styled.View`
@@ -58,7 +58,7 @@ const StyledViewControlPanelInactive = styled.View`
 `;
 
 const StyledTextMainItem = styled.Text`
-    font-size: 80; 
+    font-size: ${(props) => props.textScale*30};
     color: #ffffff;
     font-family: AvenirNextCondensed-Medium;
 `;
@@ -98,28 +98,25 @@ const StyledBtnStop = styled(StyledBtnControlPanel)`
     background-color: #f95995;
 `
 
-function createFirstTick() {
-    var lineData = [ { "x": 200,   "y": 220},  { "x": 15,  "y": 220},];
-     var lineFunction = d3.line()
-                         .x(function(d) { return d.x; })
-                         .y(function(d) { return d.y; })
-                         .curve(d3.curveLinear); 
-     console.log(lineFunction(lineData));
-    return lineFunction(lineData);
-   }
-
 export default class ActivityPopUp extends Component {
 
     constructor(props){
         super(props);
         this.closePopUp = this.props.closePopUp;
+        this.tickLength = 7;
+        this.screenDimensions = Dimensions.get('window');
+        this.scale = this.screenDimensions.scale;
+        this.surfaceWidth = this.screenDimensions.width*4/5
+        this.surfaceHeight = this.screenDimensions.height*6/15
+        this.surfaceDiff = this.surfaceWidth > this.surfaceHeight ?  this.surfaceWidth - this.surfaceHeight : null;
+        this.centroidX = (this.surfaceWidth)/2;
+        this.centroidY = this.surfaceDiff ? (this.surfaceHeight)/2 + (this.surfaceDiff) : (this.surfaceHeight)/2;
         this.state = {
             isPaused: false,
-            dFirstTick: createFirstTick(),
         };
-
     }
 
+    
     onPause = () => {
         this.setState({isPaused: true});
     }
@@ -132,25 +129,54 @@ export default class ActivityPopUp extends Component {
         this.closePopUp();
     }
 
-    drawTick(){
-        const circleAngle = 180;
-        const numberOfTick = 43;
+    createFirstTickPath(centroidX, centroidY) {
+        // 180度半圓形
+        // var lineData = [ { "x": centroidX,   "y": centroidY},  { "x": 15,  "y": centroidY + 40},];
+
+        // 270度扇型圓
+        // var lineData = [ 
+        //     { "x": centroidX,   "y": centroidY},  
+        //     { "x": centroidX - centroidX/Math.sqrt(2),  "y": centroidY + centroidX/Math.sqrt(2)},
+        // ];
+
+        // 240度扇型圓
+        // var lineData = [ 
+        //     { "x": centroidX,   "y": centroidY},  
+        //     { "x": centroidX - (centroidX/2*Math.sqrt(3)),  "y": centroidY + centroidX/2},
+        // ];
+
+        // 210度扇型圓
+        var lineData = [ 
+            { "x": centroidX,   "y": centroidY},  
+            { "x": centroidX - (centroidX/(Math.sqrt(6)+Math.sqrt(2)))*(Math.sqrt(3)+2),  "y": centroidY + (centroidX/(Math.sqrt(6)+Math.sqrt(2)))},
+        ];
+        var lineFunction = d3.line()
+                            .x(function(d) { return d.x; })
+                            .y(function(d) { return d.y; })
+                            .curve(d3.curveLinear); 
+        return lineFunction(lineData);
+    }
+    
+    drawTick(circleAngle, numberOfTick){
         const tickAngle = circleAngle/numberOfTick;
+        const dFirstTick = this.createFirstTickPath(this.centroidX, this.centroidY);
         let arcGenerator = d3.arc();
+
+        // Pie Mask to hide tick arms
         const dCircle = arcGenerator({
           startAngle: 1.5 * Math.PI,
-          endAngle: 2.5 * Math.PI,
+          endAngle: 3.5 * Math.PI,
           innerRadius: 0,
-          outerRadius: 178,
-        }); // "M0,-100A100,100,0,0,1,100,0L0,0Z"
+          outerRadius: this.centroidX-this.tickLength,
+        });
         return (
             <React.Fragment>
                 {
                     [...Array(numberOfTick+1).keys()].map(element => {
                        return <ART.Shape 
-                            d={this.state.dFirstTick} 
+                            d={dFirstTick} 
                             stroke="#a3a1af" strokeWidth={1}
-                            transform={new ART.Transform().rotate(element*tickAngle, 200, 220)}
+                            transform={new ART.Transform().rotate(element*tickAngle, this.centroidX, this.centroidY)}
                             key={element}
                         />
                     })
@@ -159,21 +185,21 @@ export default class ActivityPopUp extends Component {
                     d={dCircle} 
                     stroke="#36454f"
                     fill="#36454f"
-                    transform={new ART.Transform().translate(200, 220)}
+                    transform={new ART.Transform().translate(this.centroidX, this.centroidY)}
                 />
             </React.Fragment>
         )
     }
 
-    drawProgress(){
+    drawProgress(progress){
         const path = d3.path();
-        path.arc(0,0,182, 1 * Math.PI, 1.2 * Math.PI, false)
+        path.arc(0,0,this.centroidX - this.tickLength/2, 0.92 * Math.PI, progress * Math.PI, false)
         return (
             <ART.Shape 
                 d={path.toString()} 
                 stroke="#82C3B8"
-                strokeWidth={8}
-                transform={new ART.Transform().translate(200, 220)}
+                strokeWidth={this.tickLength}
+                transform={new ART.Transform().translate(this.centroidX, this.centroidY)}
             />
         )
     }
@@ -195,12 +221,12 @@ export default class ActivityPopUp extends Component {
                         <Text style={{color: '#ffffff'}}>跑步中</Text>
                     </StyledViewTopBar>
                     <StyledViewMainItemContainer>
-                        <ART.Surface width={400} height={400}>
-                            {this.drawTick()}
-                            {this.drawProgress()}
+                        <ART.Surface width={this.surfaceWidth} height={this.surfaceHeight}>
+                            {this.drawTick(210, 42)}
+                            {this.drawProgress(1.2)}
                         </ART.Surface>
                         <StyledViewMainItem>
-                            <StyledTextMainItem>00:03:21</StyledTextMainItem>
+                            <StyledTextMainItem textScale={this.scale}>00:03:21</StyledTextMainItem>
                         </StyledViewMainItem>
                     </StyledViewMainItemContainer>
                     <StyledViewSubItemContainer>
